b24683297cb499dff28dff8302d77e08d3b033dc
leeleegabriel <lgv0001@auburn.edu>
2019-09-16 20:52:12 -0500

    fixing pi

--- pi/run.py
+++ pi/run.py
@@ -9,7 +9,7 @@ port = "/dev/ttyAMA0"
 user = '@realDonaldTrump'
 
 def displayTweet(tweet):
-	with serial.Serial(port, 9600) as ser:
+	with serial.Serial(port, 115200) as ser:
 		ser.write(tweet)
 	print('test2')
 
