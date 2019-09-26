6c23de15089c4ba63d98619d77eeb368990a6168
Lee Vanrell <lgv0001@auburn.com>
2019-09-26 18:48:38 -0500

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -23,7 +23,11 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(16, 16, PIN,
+//Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(16, 16, PIN,
+//  NEO_MATRIX_TOP    + NEO_MATRIX_LEFT +
+//  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+//  NEO_GRB            + NEO_KHZ800);
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, 2, 1, PIN,
   NEO_MATRIX_TOP    + NEO_MATRIX_LEFT +
   NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
   NEO_GRB            + NEO_KHZ800);
