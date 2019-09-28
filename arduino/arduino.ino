9520346fdd663743c83914e777af628ec595768f
Lee Vanrell <leegvanrell@gmail.com>
2019-09-28 12:12:41 -0500

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -6,25 +6,6 @@
 
 #define PIN 3
 
-// MATRIX DECLARATION:
-// Parameter 1 = width of NeoPixel matrix
-// Parameter 2 = height of matrix
-// Parameter 3 = pin number (most are valid)
-// Parameter 4 = matrix layout flags, add together as needed:
-//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
-//     Position of the FIRST LED in the matrix; pick two, e.g.
-//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
-//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
-//     rows or in vertical columns, respectively; pick one or the other.
-//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns proceed
-//     in the same order, or alternate lines reverse direction; pick one.
-//   See example below for these values in action.
-// Parameter 5 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-
 const char* ssid     = "";
 const char* password = "";
 ESP8266WebServer server(80);
@@ -35,13 +16,12 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, 2, 1, PIN,
   NEO_GRB            + NEO_KHZ800);
 
 String display = ""; 
-String post = "";
 int x = matrix.width();
 int c = 0;
 int refresh = 35;
 
 void setup() {
-  Serial.begin(115200);
+//  Serial.begin(115200);
   WiFi.begin(ssid, password);
   matrix.begin();
   matrix.setTextWrap(false);
@@ -53,13 +33,13 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     display = "Waiting to connect . . . ";
-    Serial.println(display);
-//    matrix_scroll(25);
+//    Serial.println(display);
+    matrix_scroll(25);
   }
 
   display = "Connected! " + WiFi.localIP().toString();
-//  matrix_scroll(27);
-  Serial.println(display);
+  matrix_scroll(27);
+//  Serial.println(display);
   server.on("/body", handleBody);
   server.begin();
 }
@@ -73,10 +53,10 @@ void handleBody() {
     server.send(200, "text/plain", "Body not received");
     return;
  }
- post = server.arg("plain");
- server.send(200, "text/plain", "Body received\n" + post + "\n");
- Serial.println(post);
-// matrix_scroll();
+ display = server.arg("plain");
+ server.send(200, "text/plain", "Body received\n" + display + "\n");
+// Serial.println(display);
+ matrix_scroll(300);
 }
 
 void matrix_scroll(int c) {
