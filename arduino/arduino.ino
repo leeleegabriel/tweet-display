1367982eb9f1f5707e510175b163b4f73b3d37dc
Lee Vanrell <leegvanrell@gmail.com>
2019-09-28 10:29:52 -0500

    modified to use wifi/ESP8266

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,6 +1,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
 #define PIN 3
 
@@ -23,45 +25,67 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 
-//Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(16, 16, PIN,
-//  NEO_MATRIX_TOP    + NEO_MATRIX_LEFT +
-//  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
-//  NEO_GRB            + NEO_KHZ800);
+const char* ssid     = "";
+const char* password = "";
+ESP8266WebServer server(80);
+
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, 2, 1, PIN,
   NEO_MATRIX_TOP    + NEO_MATRIX_LEFT +
   NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
   NEO_GRB            + NEO_KHZ800);
 
-long baud = 115200;
-String a = ""; 
+String display = ""; 
+String post = "";
 int x = matrix.width();
+int c = 0;
 int refresh = 35;
-bool display_msg = false;
 
 void setup() {
-  Serial.begin(baud);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
   matrix.begin();
   matrix.setTextWrap(false);
   matrix.setBrightness(30);
   matrix.setTextColor(matrix.Color(255, 0, 0));
   matrix.fillScreen(0);
   matrix.show();
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    display = "Waiting to connect . . . ";
+    Serial.println(display);
+//    matrix_scroll(25);
+  }
+
+  display = "Connected! " + WiFi.localIP().toString();
+//  matrix_scroll(27);
+  Serial.println(display);
+  server.on("/body", handleBody);
+  server.begin();
 }
 
 void loop() {
-  display_msg = false;
+  server.handleClient();
+}
+
+void handleBody() {
+  if (server.hasArg("plain")== false){ //Check if body received
+    server.send(200, "text/plain", "Body not received");
+    return;
+ }
+ post = server.arg("plain");
+ server.send(200, "text/plain", "Body received\n" + post + "\n");
+ Serial.println(post);
+// matrix_scroll();
+}
+
+void matrix_scroll(int c) {
   x = matrix.width();
-  while(Serial.available()) {
-    a = Serial.readString();
-    display_msg = true;
-  }
-  //while(display_msg && --x > ((int)a.length() * -9)) {
-  while(display_msg && --x > ((280+20) * -12)) { 
+  while(--x > (c * -12)) { 
     matrix.fillScreen(0);
     matrix.setCursor(x, 0);
-    matrix.print(a);
+    matrix.print(display);
     matrix.show();
     delay(refresh);
-  }
-  delay(refresh*5);
+  }  
 }
